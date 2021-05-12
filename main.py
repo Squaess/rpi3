f33a8ee7192487f443155359adfdb88012e0f419
Bartosz Banasik <silentnauscopy@pop-os.localdomain>
2021-05-12 21:26:40 +0200

    Working on simeple message

--- main.py
+++ main.py
@@ -47,3 +47,44 @@ print("Printing Dupa")
 lcd.message = "Dupa"
 print("Sleeping")
 time.sleep(4)
+# Turn backlight on
+lcd.backlight = True
+# Print a two line message
+lcd.message = "Hello\nCircuitPython"
+# Wait 5s
+time.sleep(5)
+lcd.clear()
+# Print two line message right to left
+lcd.text_direction = lcd.RIGHT_TO_LEFT
+lcd.message = "Hello\nCircuitPython"
+# Wait 5s
+time.sleep(5)
+# Return text direction to left to right
+lcd.text_direction = lcd.LEFT_TO_RIGHT
+# Display cursor
+lcd.clear()
+lcd.cursor = True
+lcd.message = "Cursor! "
+# Wait 5s
+time.sleep(5)
+# Display blinking cursor
+lcd.clear()
+lcd.blink = True
+lcd.message = "Blinky Cursor!"
+# Wait 5s
+time.sleep(5)
+lcd.blink = False
+lcd.clear()
+# Create message to scroll
+scroll_msg = "<-- Scroll"
+lcd.message = scroll_msg
+# Scroll message to the left
+for i in range(len(scroll_msg)):
+    time.sleep(0.5)
+    lcd.move_left()
+lcd.clear()
+lcd.message = "Going to sleep\nCya later!"
+time.sleep(3)
+# Turn backlight off
+lcd.backlight = False
+time.sleep(2)
