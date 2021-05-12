1e785f8c9fc4973a6c46e67347a1d575471b067f
Bartosz Banasik <silentnauscopy@pop-os.localdomain>
2021-05-12 19:22:05 +0200

    Init

---
+++ main.py
@@ -0,0 +1,28 @@
+import board
+import digitalio
+import adafruit_character_lcd.character_lcd as character_lcd
+
+lcd_rs = digitalio.DigitalInOut(board.D7)
+lcd_en = digitalio.DigitalInOut(board.D8)
+lcd_d7 = digitalio.DigitalInOut(board.D12)
+lcd_d6 = digitalio.DigitalInOut(board.D11)
+lcd_d5 = digitalio.DigitalInOut(board.D10)
+lcd_d4 = digitalio.DigitalInOut(board.D9)
+lcd_backlight = digitalio.DigitalInOut(board.D13)
+
+lcd_columns = 16
+lcd_rows = 2
+
+lcd = character_lcd.Character_LCD_Mono(
+    lcd_rs,
+    lcd_en,
+    lcd_d4,
+    lcd_d5,
+    lcd_d6,
+    lcd_d7,
+    lcd_columns,
+    lcd_rows,
+    lcd_backlight
+)
+
+lcd.message = "Hello\nCircuitPython"
