db63d0c0f40df2bf64eb57ea6baf2e17bfc68f51
Bartosz Banasik <silentnauscopy@pop-os.localdomain>
2021-05-12 19:33:53 +0200

    Working on simeple message

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import board
+import time
 import digitalio
 import adafruit_character_lcd.character_lcd as character_lcd
 
@@ -13,16 +14,22 @@ lcd_backlight = digitalio.DigitalInOut(board.D13)
 lcd_columns = 16
 lcd_rows = 2
 
-lcd = character_lcd.Character_LCD_Mono(
-    lcd_rs,
-    lcd_en,
-    lcd_d4,
-    lcd_d5,
-    lcd_d6,
-    lcd_d7,
-    lcd_columns,
-    lcd_rows,
-    lcd_backlight
-)
+# lcd = character_lcd.Character_LCD_Mono(
+#     lcd_rs,
+#     lcd_en,
+#     lcd_d4,
+#     lcd_d5,
+#     lcd_d6,
+#     lcd_d7,
+#     lcd_columns,
+#     lcd_rows,
+#     lcd_backlight
+# )
+lcd = character_lcd.Character_LCD()
 
+lcd.clear()
 lcd.message = "Hello\nCircuitPython"
+time.sleep(4)
+lcd.clear()
+lcd.message("Dupa")
+time.sleep(4)
