e5e71434d30f7b9ab80d53e83b70553b71c3838c
Bartosz Banasik <silentnauscopy@pop-os.localdomain>
2021-05-12 19:34:53 +0200

    Working on simeple message

--- main.py
+++ main.py
@@ -14,18 +14,18 @@ lcd_backlight = digitalio.DigitalInOut(board.D13)
 lcd_columns = 16
 lcd_rows = 2
 
-# lcd = character_lcd.Character_LCD_Mono(
-#     lcd_rs,
-#     lcd_en,
-#     lcd_d4,
-#     lcd_d5,
-#     lcd_d6,
-#     lcd_d7,
-#     lcd_columns,
-#     lcd_rows,
-#     lcd_backlight
-# )
-lcd = character_lcd.Character_LCD()
+lcd = character_lcd.Character_LCD_Mono(
+    lcd_rs,
+    lcd_en,
+    lcd_d4,
+    lcd_d5,
+    lcd_d6,
+    lcd_d7,
+    lcd_columns,
+    lcd_rows,
+    lcd_backlight
+)
+# lcd = character_lcd.Character_LCD()
 
 lcd.clear()
 lcd.message = "Hello\nCircuitPython"
